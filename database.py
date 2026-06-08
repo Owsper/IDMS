081a322545d007a5a37c546d989f752a55e62be6
chawin1 <jirapat.chayapong@gmail.com>
2026-06-08 20:56:19 +0200

    feat: insert new users into db

--- database.py
+++ database.py
@@ -19,3 +19,17 @@ def init_db():
 
     conn.commit()
     conn.close()
+
+# Insert user data into the database
+def insert_user_data(first_name, last_name, email, password_hash, phone):
+    conn = sqlite3.connect("main_db.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO users_data (first_name, last_name, email, password_hash, phone)
+        VALUES (?, ?, ?, ?, ?)
+    """, (first_name, last_name, email, password_hash, phone))
+
+    conn.commit()
+    conn.close()
+    
