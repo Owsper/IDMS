6710d911d5c50f776dda784ccb49c03f08a949b8
Owsper <rohanftw2466@gmail.com>
2026-06-11 14:14:35 +0200

    feat: user login

--- database/database.py
+++ database/database.py
@@ -55,9 +55,28 @@ def fetch_unique_email(email):
         return True # EMAIL EXIST
 
 
-
-
+def user_login(email, password):
+    conn = sqlite3.connect("main_db.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
     
+
+    cursor.execute("SELECT email, password_hash FROM users_data WHERE email = ?", (email,))
+    user_data = cursor.fetchone()
+    conn.close()
+
+    if user_data is None:
+        return False
+    else:
+        stored_password_hash = user_data["password_hash"]
+        return bcrypt.checkpw(password.encode('utf-8'), stored_password_hash.encode('utf-8'))
+
+
+
+
+
+
+
 # Update user data in the database based on the verified email
 def update_user_data(verified_email ,first_name, last_name, email, password_hash, phone):
     conn = sqlite3.connect("main_db.db")
