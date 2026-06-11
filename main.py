083d6a3733d91332f90470ec531b73f268837d3b
Owsper <rohanftw2466@gmail.com>
2026-06-11 03:26:44 +0200

    feat: returns error when empty inputs

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
-from IDMS.database.database import insert_user_data
+from database.database import insert_user_data
 
 app = Flask(__name__, template_folder="frontend/pages")
 
 @app.route("/", methods=["GET", "POST"])
 def register_user():
+
+
+    
     if request.method == "POST":
         full_name = request.form["full_name"]
         email = request.form["email"]
@@ -12,15 +15,22 @@ def register_user():
         password = request.form["password"]
         confirm_password = request.form["confirm_password"]
 
-        if password != confirm_password:
-            return "Passwords do not match"
+        if not full_name or not email or not member_type or not password or not confirm_password:
+            return render_template(
+                "RegisterPage.html",
+                error="All fields are required"
+            )
+                
 
-        insert_user_data(full_name, email, member_type, password, confirm_password)
-
-        return "Registration successful"
+    
 
 
-    return render_template("RegisterPage.html")
+
+
+ 
+
+
+    return render_template("RegisterPage.html", )
 
 if __name__ == "__main__":
     app.run(debug=True)
